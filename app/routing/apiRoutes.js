f01cb2ba92798ebc852fd181ba3206f1a88d2e58
StephBruyere <stephbruyeredesigns@gmail.com>
2017-10-17 18:12:47 -0400

    adding app

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,23 +1,24 @@
 //LINKS TO DATA SOURCE
 var path = require('path');
-var friends = require("../data/friends");
+var petList = require("../data/friends.js");
 //ROUTING
 module.exports = function(app){
 app.get("/api/friends", function(req,res){
-		res.json(friends);
+		res.json(petList);
   });
 
 //POST REQUEST
 app.post("/api/friends", function(res, req) {
-    var newScore = req.body.scores;
+    var newPlayer = req.body;
+    var newScore = newPlayer.scores;
     var arr = [];
     var count = 0;
     var match = 0;
 
-	for (var i = 0; i < friends.length; i++) {
+	for (var i = 0; i < petList.length; i++) {
 		var diff = 0;
 		for (var j = 0; j < newScore.length; j++) {
-			diff +=(MATH.abs(parseInt(friends[i].scores[j]) - parseInt(newScore[j])));
+			diff +=(MATH.abs(parseInt(petList[i].scores[j]) - parseInt(newScore[j])));
 		}
 		arr.push(diff);
 	}
@@ -30,9 +31,6 @@ app.post("/api/friends", function(res, req) {
 
 var dog = friends[match];
 res.json(dog);
-friends.push(req.body);
+petList.push(req.body);
 });
 };
-
-console.log(friends);
-console.log("test");
